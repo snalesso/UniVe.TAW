5ae0bd2227f093e7221100242414a24c6e4ec6b2
Sergio Nalesso <nalesso.sergio@outlook.com>
2018-02-26 20:42:11 +0100

    Add project files.

---
+++ UniVe.TAW.Server/server.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = require("http");
+var port = process.env.port || 1632;
+console.log("Server starting ...");
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+}).listen(port);
+console.log("Server online @ http://127.0.0.1:" + port + "/");
+//# sourceMappingURL=server.js.map
---
+++ UniVe.TAW.Server/server.ts
@@ -0,0 +1,12 @@
+﻿import http = require('http');
+
+var port = process.env.port || 1632
+
+console.log("Server starting ...");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+}).listen(port);
+
+console.log("Server online @ http://127.0.0.1:" + port + "/");
