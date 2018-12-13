6b002ed7ef9946260a02b63f2e29bc8f6d7a1089
Sergio <nalesso.sergio@gmail.com>
2018-12-13 18:33:04 +0100

    movde gulp importing to global exporting from .WebService uninstalled unused gulp packages updated workspace file updated docs

--- UniVe.TAW.WebService/gulpfile.ts
+++ UniVe.TAW.WebService/gulpfile.ts
@@ -1,42 +1,107 @@
 import * as gulp from 'gulp';
-import * as gulpTs from 'gulp-typescript';
-import * as path from 'path';
-// import * as fs from 'fs';
+import * as undertaker from 'undertaker';
+import gulpPrint from 'gulp-print';
 import chalk from 'chalk';
+var del = require('del');
+//var vinylPaths = require('vinyl-paths');
+import * as path from 'path';
 
-const fs = require('fs');
-const gulpPrint = require('gulp-print').default;
+interface IExportBatch {
+    projectName: string,
+    sourceFolder: string,
+    destFolderName: string,
+    fileNames: string[]
+}
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json'));
-const tsProject = gulpTs.createProject('tsconfig.json');
-
-const copyTs = true;
-const copyDTs = false;
-const copyJs = false;
-
-const exportSourcePaths = [
-    "./src/DTOs"
+const exportDestFolderName = "/src/assets/unive.taw.webservice";
+const exportBatches: IExportBatch[] = [
+    {
+        projectName: "UniVe.TAW.WebSite",
+        sourceFolder: "./src",
+        fileNames: [
+            "/infrastructure/net.ts",
+            "/infrastructure/identity.ts",
+            "/infrastructure/game.ts",
+            "/infrastructure/game.client.ts",
+            "/infrastructure/chat.ts",
+            "/infrastructure/utils.ts",
+            "/application/DTOs.ts",
+            "/application/routing/RoutingParamKeys.ts",
+            "/application/services/ServiceEventKeys.ts"
+        ],
+        destFolderName: exportDestFolderName
+    },
+    {
+        projectName: "UniVe.TAW.Mobile",
+        sourceFolder: "./src",
+        fileNames: [
+            "/infrastructure/net.ts",
+            "/infrastructure/identity.ts",
+            "/infrastructure/game.ts",
+            "/infrastructure/game.client.ts",
+            "/infrastructure/chat.ts",
+            "/infrastructure/utils.ts",
+            "/application/DTOs.ts",
+            "/application/routing/RoutingParamKeys.ts",
+            "/application/services/ServiceEventKeys.ts"
+        ],
+        destFolderName: exportDestFolderName
+    },
+    {
+        projectName: "UniVe.TAW.Windows",
+        sourceFolder: "./src",
+        fileNames: [
+            "/infrastructure/net.ts",
+            "/infrastructure/identity.ts",
+            "/infrastructure/game.ts",
+            "/infrastructure/game.client.ts",
+            "/infrastructure/chat.ts",
+            "/infrastructure/utils.ts",
+            "/application/DTOs.ts",
+            "/application/routing/RoutingParamKeys.ts",
+            "/application/services/ServiceEventKeys.ts"
+        ],
+        destFolderName: exportDestFolderName
+    }
 ];
 
-const exportDestinationPaths = [
-    // D:\Dev\Repos\GitHub\snalesso\UniVe.TAW\UniVe.TAW.WebSite\src\assets\scripts
-    , '../UniVe.TAW.WebSite/src/assets/scripts'
-]
-    .map(dirPath => path.join(dirPath, packageJson.name));
-
-gulp.task('export', function () {
-
-    let missionName = "Copy of " + packageJson.name;
-
-    exportDestinationPaths.forEach(dfp => {
-        gulp.src(exportSourcePaths)
-            .pipe(gulpPrint(path => chalk.yellow("Copying ") + path + " to ..."))
-            .pipe(gulp.dest(sourceFile => {
-                console.log(JSON.stringify(sourceFile));
-                let p = path.join(dfp, sourceFile.base, sourceFile.basename);
-                process.stdout.write(p + " ...");
-                return p;
-            }))
-            .pipe(gulpPrint(path => chalk.green(" DONE")));
+const cleanFuncs: undertaker.Task[] = exportBatches.map(
+    eb => {
+        const func: undertaker.Task = (cb) => {
+            const dests = eb.fileNames.map(ed => path.join("..", eb.projectName, eb.destFolderName));
+            console.log(chalk.yellow("Cleaning") + " " + chalk.blueBright(eb.projectName) + "'s dependencies ... ");
+            return del(dests, { force: true });
+        };
+        func["displayName"] = "clean:" + eb.projectName;
+        return func;
     });
-});
+
+const cleanAllTask = gulp.series(...cleanFuncs);
+
+const exportFuncs: undertaker.Task[] = exportBatches.map(
+    eb => {
+        const func: undertaker.Task = (cb) => {
+
+            console.log(chalk.green("Exporting to ") + eb.projectName + ":");
+
+            //const sourceFilePaths = exportDestination.fileNames.map(fileName => path.join(exportDestination.sourceFolder, fileName));
+
+            return gulp
+                .src(eb.fileNames, { root: eb.sourceFolder })
+                .pipe(gulpPrint(fileName => chalk.blue("- ") + fileName + " ..."))
+                .pipe(gulp.dest(file => {
+                    const destFilePath = file.base.substr(path.join(file.cwd, eb.sourceFolder).length);
+                    //console.log(destFilePath);
+                    //console.log(file.base);
+                    return path.join("..", eb.projectName, eb.destFolderName, destFilePath);
+                }))
+                //.pipe(gulpPrint(chalk.green("DONE")))
+                ;
+        };
+        func["displayName"] = "export-to:" + eb.projectName;
+        return func;
+    });
+
+const exportAllTask = gulp.series(...exportFuncs);
+
+gulp.task("export-dependencies", gulp.series(cleanAllTask, exportAllTask));
