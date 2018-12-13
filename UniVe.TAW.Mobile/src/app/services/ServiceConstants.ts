ebdbd29c5da552983697d5fd6dd6568298c57f53
Sergio <nalesso.sergio@gmail.com>
2018-12-13 22:48:06 +0100

    updated android hoster IP

--- UniVe.TAW.Mobile/src/app/services/ServiceConstants.ts
+++ UniVe.TAW.Mobile/src/app/services/ServiceConstants.ts
@@ -1,5 +1,5 @@
 export default class ServiceConstants {
-    public static readonly ServerAddress = "http://127.0.0.1:1632";
+    public static readonly ServerAddress = "http://10.0.2.2:1632";
     // TODO: remove storage of clear Username & PW
     // public static readonly AccessCredentials_Username = "AccessCredentials_Username";
     // public static readonly AccessCredentials_Password = "AccessCredentials_Password";
