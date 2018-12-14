e1c494a40e5558bb5637e0cd1918944b6c4672b4
Sergio <nalesso.sergio@gmail.com>
2018-12-14 03:01:53 +0100

    added .Mobile ionic cordova run android task

--- UniVe.TAW.Mobile/src/app/services/ServiceConstants.ts
+++ UniVe.TAW.Mobile/src/app/services/ServiceConstants.ts
@@ -1,8 +1,3 @@
 export default class ServiceConstants {
-    public static readonly ServerAddress = "http://10.0.2.2:1632";
-    // TODO: remove storage of clear Username & PW
-    // public static readonly AccessCredentials_Username = "AccessCredentials_Username";
-    // public static readonly AccessCredentials_Password = "AccessCredentials_Password";
-    // public static readonly AccessTokenKey = "acces_token";
-    // public static readonly LoggedUserJWTPayload = "LoggedUserJWTPayload";
+    public static readonly ServerAddress = "http://" + (process.env.ServerAddress || "127.0.0.1") + ":1632";
 }
--- UniVe.TAW.Windows/src/app/services/ServiceConstants.ts
+++ UniVe.TAW.Windows/src/app/services/ServiceConstants.ts
@@ -1,8 +1,3 @@
 export default class ServiceConstants {
     public static readonly ServerAddress = "http://127.0.0.1:1632";
-    // TODO: remove storage of clear Username & PW
-    // public static readonly AccessCredentials_Username = "AccessCredentials_Username";
-    // public static readonly AccessCredentials_Password = "AccessCredentials_Password";
-    // public static readonly AccessTokenKey = "acces_token";
-    // public static readonly LoggedUserJWTPayload = "LoggedUserJWTPayload";
 }
